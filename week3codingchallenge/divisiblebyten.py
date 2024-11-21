f440abf150c07773fed21acc3eb185ec009245d5
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-11-21 08:56:31 +0300

    Week 3 coding challenge

---
+++ week3codingchallenge/divisiblebyten.py
@@ -0,0 +1,8 @@
+def divisible_by_ten(num):
+    nums = num % 10
+    if nums == 0:
+        print("True")
+    else:
+        print("False") 
+
+divisible_by_ten(25)           
---
+++ week3codingchallenge/large_power.py
@@ -0,0 +1,8 @@
+def large_power(base, exponent):
+    confirm = base ** exponent
+    if confirm > 5000 :
+        print("True")
+    else:
+        print("False")  
+
+large_power(5, 2)          
