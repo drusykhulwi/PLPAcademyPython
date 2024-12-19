fb18ee9843fa5039521d5e436a497ef611d69c0d
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-12-19 19:22:02 +0300

    Stylings

--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -118,7 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+
+# Directories where Django will look for static files during development
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Your static folder is directly under the project root
+]
+
+# Directory to collect static files when using 'collectstatic'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Collect static files here when deploying
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
